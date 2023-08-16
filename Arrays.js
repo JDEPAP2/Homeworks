7ee8e8bba1dc78d452a9fa000f9f5d9ff6764109
JOSE DAVID ESCOBAR PRADA <jose.escobar@uao.edu.co>
2023-08-15 21:04:07 -0500

    a

--- Arrays.js
+++ Arrays.js
@@ -1,10 +1,5 @@
-
-//Length: Corresponde al tamaño del arreglo
-var arr = [1,2,3,4,5]
-console.log(`Lenght/ El arreglo tiene ${arr.length} elementos`)
-
 //At:
-arr = ["Pancho","Sandra","Maria","Ana","Alba"]
+var arr = ["Pancho","Sandra","Maria","Ana","Alba"]
 console.log()
 
 //Concat:
@@ -65,6 +60,118 @@ console.log(`La lista resultante es ${arr.flat()}`)
 arr = ["Aqui no", "Aqui tampoco", "Aqui menos","Insertar Aqui", "Ni aqui"]
 console.log(`La lista resultante es ${arr.flatMap((v)=>v=="Insertar Aqui"?["se inserto este", "este tambien"]:v)}`)
 
+//ForEach:
 arr = [1,1,1,1,1,1]
 arr.forEach((v,i)=>arr[i]= v+i)
 console.log(`La lista resultante es ${arr}`)
+
+//Includes:
+arr = ["Camila","Camilo","Catalino","Victorino"]
+console.log(arr.includes("Camila"))
+console.log(arr.includes("Cam"))
+
+//IndexOf:
+arr = [1,2,1,3,4,5,6,1]
+console.log(arr.indexOf(1))
+console.log(arr.indexOf(1,3))
+
+//IsArray:
+arr = [1,2,3,4,5]
+test = {1:"",2:""}
+console.log(Array.isArray(arr))
+console.log(Array.isArray(test))
+
+//Join
+arr = ["Hola", "como", "estas"]
+console.log(arr.join())
+console.log(arr.join(" "))
+
+//Keys
+arr = ["pos0","pos1","pos2"]
+for (const key of arr.keys()) {
+  console.log(key);
+}
+
+//LastIndexOf
+arr = ["este","no","este no","este"]
+console.log(arr.lastIndexOf("este"))
+
+//Length: Corresponde al tamaño del arreglo
+arr = [1,2,3,4,5]
+console.log(`El arreglo tiene ${arr.length} elementos`)
+
+//Map
+arr = [2,3,4,5,6]
+console.log(arr.map((v)=>v%2 == 0?"Es par":"Es impar"))
+
+//Pop
+arr = [2,3,4,5,6]
+console.log(arr.pop())
+console.log(arr)
+
+//Push
+arr = ["Hola", "que"]
+arr.push("Mas")
+console.log(arr)
+
+//Reduce
+arr = ["h","o","l","a"]
+console.log(arr.reduce((a,b)=>a+b))
+
+//ReduceRight
+arr = ["a","l","o","h"]
+console.log(arr.reduceRight((a,b)=>a+b))
+
+//Reverse
+arr = [5,4,3,2,1]
+console.log(arr.reverse())
+
+//Shift
+arr = ["JKGBH","Hola","como","estas"]
+console.log(arr.shift())
+console.log(arr)
+
+//Slice
+arr = [3,2,1,0,1,2]
+console.log(arr.slice(0,3))
+
+//Some
+arr = ["Pedro","Anastasio","Patroclo"]
+console.log(arr.some(v=>v.includes("e"))?"Si hay palabras con el":"No hay palabras con e")
+
+//Sort
+arr = ["Dante","Anabel","Brayan","Camilo"]
+console.log(arr.sort())
+
+//Splice
+arr = ["Falta","an","elemento","aqui"]
+console.log(arr.splice(5,0,"xd"))
+console.log(arr.splice(1,1,"un"))
+console.log(arr)
+
+//ToLocaleString
+arr = [Date("15/08/2023")]
+console.log(arr.toLocaleString('es',{timeZone:"UTC"}))
+console.log(arr.toLocaleString('en',{timeZone:"UTC"}))
+console.log(arr.toLocaleString('fr',{timeZone:"UTC"}))
+// toReversed
+// : 
+// ƒ toReversed()
+// toSorted
+// : 
+// ƒ toSorted()
+// toSpliced
+// : 
+// ƒ toSpliced()
+// toString
+// : 
+// ƒ toString()
+// unshift
+// : 
+// ƒ unshift()
+// values
+// : 
+// ƒ values()
+// with
+// : 
+// ƒ with()
