85a2f7143ab5d3cc7f088e9b3545b3dad96cb54d
JOSE DAVID ESCOBAR PRADA <jose.escobar@uao.edu.co>
2023-08-22 20:52:17 -0500

    inClass

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import FirstApp from "./FirstApp";
+
+const App = () => {
+  return <FirstApp/>
+}
+
+export default App;
---
+++ src/FirstApp.jsx
@@ -0,0 +1,10 @@
+
+const FirstApp = () => {
+    const title = "Juanes es un pato"
+    return<>
+        <h1>{title}</h1>
+        <span>Nivel 10</span>
+    </>
+}
+
+export default FirstApp;
