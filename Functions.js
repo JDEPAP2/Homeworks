b4d31d1c631cb4f4d7ecebafbc2acd8929bd2c93
Jose Escobar <85772095+JDEPAP2@users.noreply.github.com>
2023-08-22 20:45:29 -0500

    Add files via upload

---
+++ Functions.js
@@ -0,0 +1,8 @@
+function numberType(num){
+    return num%2 === 0?"Es  par":"Es impar"
+}
+
+const numberTypeA = (num) => num%2 === 0?"Es par":"Es impar"
+                                                                                                                              
+console.log(numberTypeA(6))
+console.log(numberType(3))
