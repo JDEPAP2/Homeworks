8053ce186dc7fcff14e4a236da227af8e18e67e4
JOSE DAVID ESCOBAR PRADA <jose.escobar@uao.edu.co>
2023-08-24 21:31:07 -0500

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import ComponentApp from "./ComponentApp";
 import FirstApp from "./FirstApp";
 
 const App = () => {
-  return <FirstApp value={1}/>
+  return <>
+    {/* <FirstApp value={1}/> */}
+    <ComponentApp/>
+  </>
 }
 
 export default App;
