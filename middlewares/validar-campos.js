a2e67d1bcff42055f537c51d219005d7fb625cc4
JDEPAP2 <jdepap2@gmail.com>
2023-11-23 01:23:32 -0500

    17

---
+++ middlewares/validar-campos.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { validationResult } = require('express-validator');
+
+const validarCampos = (req, res = express.response, next) =>{
+    const error = validationResult(req);
+    if(!error.isEmpty()){
+        return res.status(400).json({
+            ok: false,
+            error: error.mapped()
+        })
+    }
+
+    next();
+}
+
+module.export = {validarCampos}
